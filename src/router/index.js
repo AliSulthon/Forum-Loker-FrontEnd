0fcc875ed2f4727613f3a1def6fbec6f14b5e8cd
Agam-dwi <dwiyanaagam@gmail.com>
2025-11-25 18:43:45 +0700

    Update v1

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,40 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import RegisterView from '../views/Register.vue';
-import LoginView from '../views/Login.vue';
+import Login from '../views/Login.vue';
 import ArticleList from '../views/ArticleList.vue';
 import ArticleCreate from '../views/ArticleCreate.vue';
+import ArticleEdit from '../views/ArticleEdit.vue';
+import ArticleDelete from '../views/ArticleDelete.vue';
 
 const routes = [
-  { path: '/register', component: RegisterView },
-  { path: '/login', component: LoginView },
-  { path: '/articles', component: ArticleList },
-  { path: '/create-article', component: ArticleCreate },
+  { path: '/register', component: Register },
+  { path: '/login', component: Login },
+  {
+  path: "/articles",
+  name: "articles",
+  component: ArticleList,
+},
+{
+  path: "/articles/create",
+  name: "article-create",
+  component: ArticleCreate,
+},
+{
+  path: "/articles/edit/:id",
+  name: "article-edit",
+  component: ArticleEdit,
+},
+{
+  path: "/articles/delete/:id",
+  name: "article-delete",
+  component: ArticleDelete,
+},
+
 ];
 
 export default createRouter({
   history: createWebHistory(),
   routes,
 });
+
